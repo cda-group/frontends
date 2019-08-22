a7355ab4c0871846a724d186c8aaa060c3b09d8d
Klas Segeljakt <klasseg@kth.se>
2019-08-22 21:33:19 +0200

    Fix xy coordinate bug

--- tests/birte_demo_4.py
+++ tests/birte_demo_4.py
@@ -9,7 +9,7 @@ import baloo as pandas
 class TestSuite2(object):
     def test2(self):
         p = beam.Pipeline()
-        height, width = 500, 300  # 500x300 points on touchpad
+        width, height = 500, 300  # 500x300 points on touchpad
         num_x_grids, num_y_grids = 5, 3  # 5x3 grids
         grid_width, grid_height = int(width / num_x_grids), int(height / num_y_grids)  # height/width of grids
 
@@ -20,9 +20,9 @@ class TestSuite2(object):
 
          | 'preprocess' >> beam.Filter(lambda e: (e[3] > 0.0) & (e[3] < 1.0))
          | 'extract timestamp' >> beam.Map(lambda e: window.TimestampedValue(e[1:4], e[0]))
-         | 'extract key' >> beam.Map(lambda e: (((e[0] / grid_height).asInt(), (e[1] / grid_width).asInt()), e[2]))
+         | 'extract key' >> beam.Map(lambda e: (((e[0] / grid_width).asInt(), (e[1] / grid_height).asInt()), e[2]))
          | 'add to pressure' >> beam.Map(lambda e: (e[0], e[1] + 0.1))
-         | 'create tumbling window' >> beam.WindowInto(window.FixedWindows(60))
+         | 'create tumbling window' >> beam.WindowInto(window.FixedWindows(6))
          | 'sum up pressures' >> beam.CombinePerKey(lambda e: pandas.Series(e).sum())
          | 'collect window as list' >> combiners.ToList()
          # | 'normalize grid' >> beam.Map(normalize)
