2143d9822bca6da1139ccd0fcdb6f9065ffb0e89
Klas Segeljakt <klasseg@kth.se>
2019-08-23 13:46:11 +0200

    Cleanup demo

--- tests/birte_demo_4.py
+++ tests/birte_demo_4.py
@@ -11,23 +11,35 @@ class TestSuite2(object):
         p = beam.Pipeline()
         width, height = 500, 300  # 500x300 points on touchpad
         num_x_grids, num_y_grids = 5, 3  # 5x3 grids
-        grid_width, grid_height = int(width / num_x_grids), int(height / num_y_grids)  # height/width of grids
+        grid_width, grid_height = int(width / num_x_grids), int(height / num_y_grids)
+        epsilon = 0.1
+        ts, x, y, z = int, float, float, float
 
         (p
-         | beam.io.ReadFromSocket('127.0.0.1:8000', beam.coders.CSVCoder()).with_output_types(
-                    #      ts,     x,     y,     z
-                    Tuple[int, float, float, float])
+         | beam.io.ReadFromSocket('127.0.0.1:8000', beam.coders.CSVCoder())
+                  .with_output_types(Tuple[ts, x, y, z])
 
-         | 'preprocess' >> beam.Filter(lambda e: (e[3] > 0.0) & (e[3] < 1.0))
-         | 'extract timestamp' >> beam.Map(lambda e: window.TimestampedValue(e[1:4], e[0]))
-         | 'extract key' >> beam.Map(lambda e: (((e[0] / grid_width).asInt(), (e[1] / grid_height).asInt()), e[2]))
-         | 'add to pressure' >> beam.Map(lambda e: (e[0], e[1] + 0.1))
-         | 'create tumbling window' >> beam.WindowInto(window.FixedWindows(6))
-         | 'sum up pressures' >> beam.CombinePerKey(lambda e: pandas.Series(e).sum())
-         | 'collect window as list' >> combiners.ToList()
-         # | 'normalize grid' >> beam.Map(normalize)
+         | 'preprocess'
+            >> beam.Filter(lambda e: (e[3] >= 0.0) & (e[3] < 1.0))
+
+         | 'extract timestamp'
+            >> beam.Map(lambda e: window.TimestampedValue(e[1:4], e[0]))
+
+         | 'extract key'
+            >> beam.Map(lambda e: (((e[0] / grid_width).asInt(), (e[1] / grid_height).asInt()), e[2]))
+
+         | 'add to pressure'
+            >> beam.Map(lambda e: (e[0], e[1] + epsilon))
+
+         | 'create tumbling window'
+            >> beam.WindowInto(window.FixedWindows(size=6))
+
+         | 'sum up pressures'
+            >> beam.CombinePerKey(lambda e: pandas.Series(e).sum())
+
+         | 'collect window as list'
+            >> combiners.ToList()
 
          | beam.io.WriteToSocket('127.0.0.1:9000', beam.coders.CSVCoder()))
 
         p.run()
-        # | 'sum up pressures' >> beam.CombinePerKey(lambda elem: (pandas.Series(elem[1]) + 0.1).sum())
